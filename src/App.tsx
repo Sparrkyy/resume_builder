d239f3f76623cf68b2c03f7211c6c54fd5a4d8e1
useStateEthan <ethancampbellhodges@gmail.com>
2021-07-02 19:04:59 -0400

    completed set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="resume_border">
+        <div className="title_line">
+            <h1>Ethan Hodges</h1>
+            <h3>Computer Science Student</h3>
+        </div>
     </div>
+
   );
 }
 
-export default App;
+export default App
