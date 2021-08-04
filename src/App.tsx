43cbe0f6fbccede2b6a3c9da410d57bc70742c99
useStateEthan <ethancampbellhodges@gmail.com>
2021-08-04 11:12:22 -0400

    updated wording and margins and fixed two spelling mistakes

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,10 @@ const content = {
 	education: {
 		universityName: "MCGILL UNIVERSITY",
 		dates: "2019-2022",
-		degreeName: "Backlors Of Arts: Computer Science",
+		gpa: "3.90",
+		degreeName: "Bachlors Of Arts: Computer Science",
 		relevantCourses:
-			"Relevant Courses: Foundation of programming, Introduction to Computer Science, Algorithms and Data Structures, Introduction to Software Systems, Software Design. Discrete Mathematics, Linear Algebra, Linear Algebra and Geometry, Calculus 1, Calculus 2.",
+			"Relevant Courses: Algorithims and Data Structures, Introduction to Software Systems, Software Design, Foundation of programming, Introduction to Computer Science, Discrete Mathematics",
 	},
 	employment: {
 		positions: [
@@ -27,10 +28,10 @@ const content = {
 				dates: "May-Sep 2021",
 				positionTitle: "Software Developer Intern",
 				bulletPoints: [
-					"Redesigned and reconstructed a web application to implement structural changes in data, including implementing a graph-like data structure to handle complex dependencies.",
+					"Designed and implemented a graph data structure using Node.js and MongoDB to handle complex data relationships that is now used across all platforms.",
+					"Played a key role in the reconstruction of a internal web app in React.js from Vue.js with over 80 commits pushed to production.",
 					"Automated data workflow with python to allow for non-technical members to safely update data, and have those changes reflected across our platforms saving countless hours of work from the technical and non-technical teams.",
-					"Played a key role in migrating and maintaining the backend after the original developer left, including creating documention for it, and adding new API end points for our various applications.",
-					"Designed the way that important data for a new service was stored as to allow for consistency across all of our platforms",
+					"Created documention for, and added end points to our API for our various applications in Node.js.",
 				],
 			},
 			{
@@ -38,9 +39,8 @@ const content = {
 				dates: "Dec 2020 - May 2021",
 				positionTitle: "Full Stack Developer Intern",
 				bulletPoints: [
-					"Played a key role in building and maintaining an internal dashboard that handles real time information.",
+					"Played a key role in building and maintaining an internal dashboard that handles real time information and was integral to creating new bussiness oppertunties.",
 					"Built a Python SDK for the dashboard to allow for manipulation of the displayed data from other programs.",
-					"Played a large role in creating new business opportunities through the above work.",
 				],
 			},
 		],
@@ -78,7 +78,7 @@ const content = {
 			},
 			{
 				skillType: "LANGAUGES",
-				skillList: "English (Native), French (Conversational)",
+				skillList: "English (Native), French (Fluent)",
 			},
 		],
 	},
@@ -91,7 +91,7 @@ function App() {
 				{/* Title */}
 				<section className='title_line'>
 					<h1 className='title_line_name'>{content.title.fullName}</h1>
-					<h3> {content.title.title} </h3>
+					{/* <h3> {content.title.title} </h3> */}
 				</section>
 				{/* Contact Info */}
 				<section className='contact_info'>
@@ -120,6 +120,7 @@ function App() {
 					subtitle={content.education.degreeName}
 					date={content.education.dates}
 				/>
+				<Subtitle subtitle={content.education.gpa} />
 				<p className='relevant_courses_text'>{content.education.relevantCourses}</p>
 			</InformationalBlock>
 			<InformationalBlock sectionTitle='EMPLOYMENT'>
@@ -157,6 +158,17 @@ function App() {
 	);
 }
 
+interface subtitleContent {
+	subtitle: string;
+}
+
+const Subtitle: FC<subtitleContent> = (props) => {
+	return (
+		<div className='date_and_title_subtitle'>
+			<span className='date_italic_text'>{"GPA: " + props.subtitle}</span>
+		</div>
+	);
+};
 interface InformationBlockContent {
 	sectionTitle: string;
 }
